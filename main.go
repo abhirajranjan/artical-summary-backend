aadd597232f8f8aa47c6428b964a5426651e33f6
Abhiraj ranjan <abhiraj.ranjan@outlook.com>
2023-11-15 11:11:55 +0000

    aaaaaaa

--- main.go
+++ main.go
@@ -36,8 +36,8 @@ type opts map[string]http.HandlerFunc
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST,, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
 	
 	next.ServeHTTP(w, r)
